3e58f98bbd38f6aa78884ee1a20beb024a51d72f
god-of-jesters <ryzovdima@gmail.com>
2023-01-30 22:09:55 +0300

    add structure

--- pythonProject/db.py
+++ pythonProject/db.py
@@ -3,7 +3,7 @@ import sqlite3
 def ins(mes: int):
     d = sqlite3.connect('serv.db')
     cur = d.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS users(int id, string name)")
+    cur.execute("CREATE TABLE IF NOT EXISTS users(id INT, name TEXT)")
     cur.execute(f"INSERT INTO users VALUES({mes}, 'АГЫВШ')")
     d.commit()
     return "Goood"
