b810cf88e8d44d9a435172d5d6361a462dd18ee8
god-of-jesters <ryzovdima@gmail.com>
2023-02-05 18:04:37 +0300

    add structure

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -13,21 +13,11 @@ cur = db.cursor()
 def user_text(message):
     menu(bot, message)
 
-@bot.callback_query_handler(func = lambda call:True)
-def re(call):
-    if call.data == 'yes':
-        mar = types.ReplyKeyboardMarkup(resize_keyboard= True)
-        item = types.KeyboardButton('Мой ID')
-        item2 = types.KeyboardButton('Ник')
-        mar.add(item, item2)
-
-        bot.send_message(call.message.chat.id, 'Перейдите', reply_markup= mar)
-    else:
-        bot.send_message(call.message.chat.id, 'Покеда')
-
+# @bot.callback_query_handler(func = lambda call:True)
+# def re(call):
+# Вызов обработчика сообщений
 @bot.message_handler(content_types= ['text'])
 def cal(mes):
     processor(mes, bot)
-#     if mes.text == 'Мой ID':
-#         bot.send_message(mes.chat.id, f'ID: {mes.from_user.id}')
-bot.polling(none_stop=True)
+
+bot.polling(none_stop = True)
